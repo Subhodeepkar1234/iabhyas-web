86b1762440f31b7ef66a5c37fdcdb9ddb52c9203
subhodeep nandy <subhodeep@karmicksolutions.com>
2026-07-27 16:16:57 +0530

    iabhyas web first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const header = document.querySelector('.header');
+window.addEventListener('scroll', function () {
+    if (window.scrollY > 80) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
